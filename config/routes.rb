253133d85b902bd7fb8a244968499bd728bbf2a6
Andre Menezes <djdedemenezes@gmail.com>
2021-06-08 18:01:59 -0300

    routes for new and create

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
+  resources :posts, only: [:new, :create]
   resources :groups, only: [:index, :show, :new, :create]
 end
