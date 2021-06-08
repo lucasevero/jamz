1512491097c747b0bb8f37eccd4c8d2dd8cc71b0
Danilo Castro <danilogcastro@hotmail.com>
2021-06-08 12:19:24 -0300

    added validations to Skill and Instrument

--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -2,4 +2,7 @@ class Instrument < ApplicationRecord
   has_many :skills
   has_many :users, through: :skills
 
+  INSTRUMENTS = ['Guitar', 'Electric Guitar', 'Bass', 'Drums']
+
+  validates :name, inclusion: { in: INSTRUMENTS }
 end
--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,4 +1,8 @@
 class Skill < ApplicationRecord
   belongs_to :user
   belongs_to :instrument
+
+  EXPERIENCE = ['Beginner', 'Intermediate', 'Pro']
+
+  validates :experience, inclusion: { in: EXPERIENCE }
 end
