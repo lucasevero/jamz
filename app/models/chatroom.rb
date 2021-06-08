1ae82c3a3fb3ae38953c04c18c349b851cee47b1
Danilo Castro <danilogcastro@hotmail.com>
2021-06-08 11:52:39 -0300

    added associations

--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -1,2 +1,4 @@
 class Chatroom < ApplicationRecord
+  has_many :messages
+  has_many :users, through: :chat_memberships
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  has_many :messages
+  has_many :chat_memberships
+  has_many :chatrooms, through: :chat_memberships
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
