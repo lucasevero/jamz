3161e2d79d8370ce66bd2419a807148ed6a1f453
Andre Menezes <djdedemenezes@gmail.com>
2021-06-08 19:08:37 -0300

    cloudinary key on env, photo on post params, filke on form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -24,6 +24,6 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:content, :user, :group)
+    params.require(:post).permit(:content, :user, :group, photos: [])
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,6 @@ class Post < ApplicationRecord
 
   validates :content, presence: true
   validates :content, length: { minimum: 1, maximum: 140 }
+
+  has_many_attached :photos
 end
