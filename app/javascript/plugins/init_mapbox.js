6ecc499ee20c42635528699c1b2b373afab008f1
Danilo Castro <danilogcastro@hotmail.com>
2021-06-09 16:51:07 -0300

    added search to map

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -1,5 +1,7 @@
 import mapboxgl from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
+import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 
 const initMapbox = () => {
   const mapElement = document.getElementById('map');
@@ -7,7 +9,7 @@ const initMapbox = () => {
   const fitMapToMarkers = (map, markers) => {
     const bounds = new mapboxgl.LngLatBounds();
     markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-    map.fitBounds(bounds, { padding: 75, maxZoom: 5, duration: 0 });
+    map.fitBounds(bounds, { padding: 100, maxZoom: 15, duration: 0 });
   };
 
   if (mapElement) {
@@ -26,6 +28,8 @@ const initMapbox = () => {
         .addTo(map);
     });
     fitMapToMarkers(map, markers);
+    map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken,
+                                      mapboxgl: mapboxgl }));
   }
 };
 
