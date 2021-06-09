b2a9e878a9d23d8dea14b6551dab7a259656a1d1
Andre Menezes <djdedemenezes@gmail.com>
2021-06-09 17:12:03 -0300

    message can be created and sent to the channel. anchor working but still need to refresh on the other side

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,10 +1,22 @@
 class MessagesController < ApplicationController
 
   def create
-    raise
+    @chatroom = Chatroom.find(params[:chatroom_id])
     @message = Message.new(message_params)
+    @message.user = current_user
+    @message.chatroom = @chatroom
     authorize @message
+    if @message.save
+      redirect_to chatroom_path(@chatroom, anchor: "message#{@message.id}")
+    else
+      render 'chatrooms/show'
+    end
   end
 
 
+  private
+  
+  def message_params
+    params.require(:message).permit(:content, :chatroom_id)
+  end
 end
