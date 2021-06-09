0cd96c62ef8b40da807722248fc08826d99423d7
Andre Menezes <djdedemenezes@gmail.com>
2021-06-09 17:01:33 -0300

    Ok, to send message without submit btn we NEED to have content type as string. This migration did this

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_08_202837) do
+ActiveRecord::Schema.define(version: 2021_06_09_195212) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,7 +66,7 @@ ActiveRecord::Schema.define(version: 2021_06_08_202837) do
   end
 
   create_table "messages", force: :cascade do |t|
-    t.text "content"
+    t.string "content"
     t.bigint "chatroom_id", null: false
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
