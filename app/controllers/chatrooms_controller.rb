edd29222497ae66116d322139f17fd9703a02502
Andre Menezes <djdedemenezes@gmail.com>
2021-06-10 12:43:47 -0300

    chatrrom can be created from profille view using message button. Friend still dont have how to get there

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -7,15 +7,22 @@ class ChatroomsController < ApplicationController
   end
 
   def create
-    raise
     @friend = User.find(params[:user_id])
     @chatroom = Chatroom.new(name: "#{@friend.username} - #{current_user.username}")
+    @friend_membership= ChatMembership.new(user: @friend, chatroom: @chatroom)
+    @current_user_membership= ChatMembership.new(user: current_user, chatroom: @chatroom)
     authorize @chatroom
     if @chatroom.save
       redirect_to chatroom_path(@chatroom)
     else
       render "users/:id", { id: @friend.id }
     end
+  end
+
+  private
+
+  def check_chatroom
+    @chatroom = Chatroom.find_by(name: "#{@friend.username} - #{current_user.username}").exist?
   end
 
 end
