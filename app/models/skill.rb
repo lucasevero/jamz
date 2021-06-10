ccb25b8179f0c25b9754b03a344a9def1ea19444
Danilo Castro <danilogcastro@hotmail.com>
2021-06-10 18:40:57 -0300

    added column instrument to skill model

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -3,6 +3,8 @@ class Skill < ApplicationRecord
   belongs_to :instrument
 
   EXPERIENCE = ['Beginner', 'Intermediate', 'Pro']
+  INSTRUMENTS = ['Guitar', 'Electric Guitar', 'Bass', 'Drums']
 
   validates :experience, inclusion: { in: EXPERIENCE }
+  validates :instrument, inclusion: { in: INSTRUMENTS }
 end
