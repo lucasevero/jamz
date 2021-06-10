bb46d6b23c27b273ab371f62e5a73c06202cc6d9
Luca Severo <luca98.severo@gmail.com>
2021-06-10 17:11:54 -0300

    JS working in local host

--- app/javascript/plugins/init_navbar.js
+++ app/javascript/plugins/init_navbar.js
@@ -4,11 +4,15 @@ const navChatrooms = document.getElementById("nav-chatrooms")
 const navProfile = document.getElementById("nav-profile")
 
 const initNavbar = () => {
-  if (document.URL == "http://localhost:3000/users/1") {
-    navProfile.classList.add("active");
-  } else if (document.URL == "http://localhost:3000/users/feed") {
+  if (document.URL == "http://localhost:3000/users/feed") {
     navHome.classList.add("active");
-  }
+  } else if (document.URL == "http://localhost:3000/groups") {
+    navGroups.classList.add("active");
+  } else if (document.URL == "http://localhost:3000/users/1/chat_memberships") {
+    navChatrooms.classList.add("active");
+  } else if (document.URL == "http://localhost:3000/users/1") {
+    navProfile.classList.add("active");
+  };
 }
 
 export { initNavbar };
