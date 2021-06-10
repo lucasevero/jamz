40acc37a445b2ad1461f825fe996630d1a58c88a
Andre Menezes <djdedemenezes@gmail.com>
2021-06-10 18:32:13 -0300

    chat closing and opening

---
+++ app/javascript/plugins/init_userChat.js
@@ -0,0 +1,11 @@
+const userChat = () =>{
+  const trigger = document.getElementById('chat-trigger');
+  if(trigger) {
+    const target = document.getElementById('chat-target');
+
+    trigger.addEventListener('click', (event) => {
+      target.classList.toggle('d-none');
+    });
+  };
+};
+
