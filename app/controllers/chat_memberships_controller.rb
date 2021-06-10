ae3eea7ca975c4c3121f09c2e53311f532894bb8
Andre Menezes <djdedemenezes@gmail.com>
2021-06-10 15:57:18 -0300

    index return ony current_user chats

--- app/controllers/chat_memberships_controller.rb
+++ app/controllers/chat_memberships_controller.rb
@@ -1,7 +1,9 @@
 class ChatMembershipsController < ApplicationController
 
   def index
-    @chat_memberships = ChatMembership.all
+    @chat_memberships = ChatMembership.where(user: current_user)
+
+    # achar os do usuario
     authorize @chat_memberships
   end
 end
