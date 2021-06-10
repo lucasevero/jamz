eeff2ddba80c2033f079eca9b3f4800b523e386b
Andre Menezes <djdedemenezes@gmail.com>
2021-06-10 16:04:31 -0300

    this policy to creat a chatroom was something crazy to achive. record we can access friend and user ourselfs

--- app/policies/chatroom_policy.rb
+++ app/policies/chatroom_policy.rb
@@ -10,6 +10,7 @@ class ChatroomPolicy < ApplicationPolicy
   end
 
   def create?
-    true
+    # checar se o chat é com ele mesmo && se algum chatroom_id do user bate com chatroom_id do friend
+    record.chat_memberships.first != user && user.chat_memberships.select { |chat_membership| record.chat_memberships.first.user.chat_memberships.find_by(chatroom_id: chat_membership.chatroom_id)}.empty?
   end
 end
