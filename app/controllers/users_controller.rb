34f8a17f95584845782f136065ffdc8fc9e2ada8
Carla Valdivia <valdivia.carla@gmail.com>
2021-06-10 06:49:02 -0300

    Feed page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,10 @@ class UsersController < ApplicationController
   def feed
     @user = current_user
     @post = Post.new
+    @posts = @posts = Post.all.order(updated_at: :desc)
+    authorize @user
+    authorize @post
+    authorize @posts
   end
 
 
@@ -18,7 +22,7 @@ class UsersController < ApplicationController
       @users = User.all
     end
   end
-  
+
   def show
     @user = User.find(params[:id])
     @posts = @user.posts
