d50de70f03a83437c7ecceaeba974197c39ccb91
Luca Severo <luca98.severo@gmail.com>
2021-06-10 17:29:38 -0300

    Navbar now works with different user ids

--- app/javascript/plugins/init_navbar.js
+++ app/javascript/plugins/init_navbar.js
@@ -2,15 +2,16 @@ const navHome = document.getElementById("nav-home")
 const navGroups = document.getElementById("nav-groups")
 const navChatrooms = document.getElementById("nav-chatrooms")
 const navProfile = document.getElementById("nav-profile")
+const currentUserId = document.getElementById("current-user-id")
 
 const initNavbar = () => {
   if (document.URL == "http://localhost:3000/users/feed") {
     navHome.classList.add("active");
   } else if (document.URL == "http://localhost:3000/groups") {
     navGroups.classList.add("active");
-  } else if (document.URL == "http://localhost:3000/users/1/chat_memberships") {
+  } else if (document.URL == `http://localhost:3000/users/${currentUserId.innerText}/chat_memberships`) {
     navChatrooms.classList.add("active");
-  } else if (document.URL == "http://localhost:3000/users/1") {
+  } else if (document.URL == `http://localhost:3000/users/${currentUserId.innerText}`) {
     navProfile.classList.add("active");
   };
 }
