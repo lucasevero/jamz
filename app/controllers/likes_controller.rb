b2011ea825183a20a094c72dfe05acab05e3678c
Andre Menezes <djdedemenezes@gmail.com>
2021-06-11 19:18:13 -0300

    fixed typo on routes, added link to create and define create method

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,6 +1,18 @@
 class LikesController < ApplicationController
+  before_action :set_post
 
   def create
-    raise
+    @like = Like.new(user: current_user, post: @post)
+    if @like.save
+      redirect_to feed_users_path
+    else
+      render :feed_users
+    end
+  end
+
+  private 
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
