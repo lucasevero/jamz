df1829b67a97d4a7455c04e8d4864926b7bbc1a1
Andre Menezes <djdedemenezes@gmail.com>
2021-06-14 16:44:34 -0300

    using rails.ajax we can address easil to our own paths

--- app/javascript/plugins/fetchChatroom.js
+++ app/javascript/plugins/fetchChatroom.js
@@ -8,10 +8,8 @@ const actions = document.querySelectorAll('.chat-action');
 const fetchChatroom = () => {
   actions.forEach((action => {
     action.addEventListener('click', (event) => {
-    
       const newTarget = document.getElementById(`chatroom-${action.dataset.id}`)
       newTarget.style.display = 'block'
-
       Rails.ajax({
         url: `/chatrooms/${action.dataset.id}`,
         type: "get",
@@ -21,7 +19,6 @@ const fetchChatroom = () => {
           newTarget.insertAdjacentHTML('beforeend', `<div class='opened-chatroom'>${html_doc.innerHTML}</div>`)
           const id = newTarget.dataset.chatroomId;
           const formMessage = document.getElementById('messages')
-  
           consumer.subscriptions.create({ channel: "ChatroomChannel", id: id }, {
             received(data) {
               formMessage.insertAdjacentHTML("beforeend", data);
@@ -31,15 +28,6 @@ const fetchChatroom = () => {
           },
         error: function(data) {}
       })
-      
-      //fetch(`http://www.jamz.club/chatrooms/${action.dataset.id}`, { mode: 'no-cors'} )
-      // .then((response) => {
-      //   return response.text()
-      //})
-      //.then(function(html) {
-      //  let html_doc = apiParse(html)
-      //});
-      
     });
   }));
 };
