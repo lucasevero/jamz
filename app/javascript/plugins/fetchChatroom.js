dc395bddef0357dca4c618f8579f127e9949ccc1
Andre Menezes <djdedemenezes@gmail.com>
2021-06-14 13:48:54 -0300

    css begin

--- app/javascript/plugins/fetchChatroom.js
+++ app/javascript/plugins/fetchChatroom.js
@@ -3,6 +3,7 @@ import consumer from "../channels/consumer";
 const target = document.getElementById('chat-target');
 const actions = document.querySelectorAll('#chat-action');
 const current_user_id = document.getElementById('current-user-id').innerText
+const chatDiv = document.querySelectorAll('.user-chat')
 
 const fetchChatroom = () => {
   actions.forEach((action => {
@@ -10,8 +11,10 @@ const fetchChatroom = () => {
       // console.log(event.currentTarget) ||  Div which event happened
       // console.log(action.dataset.id) ||  id of the chatroom which event happened
       console.log(action)
+
       const newTarget = document.getElementById(`chatroom-${action.dataset.id}`)
-      console.log(newTarget)
+      newTarget.style.display = 'block' // Quando fechar tem que display: flex;
+      
       fetch(`http://localhost:3000/chatrooms/${action.dataset.id}`, { mode: 'no-cors'} )
        .then((response) => {
          return response.text()
@@ -36,7 +39,6 @@ const fetchChatroom = () => {
         consumer.subscriptions.create({ channel: "ChatroomChannel", id: id }, {
           received(data) {
             formMessage.insertAdjacentHTML("beforebegin", data);
-            formMessage.innerText = ""
           },
         });
       });
