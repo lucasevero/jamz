837045eb1881970adfe840791d6640ab352df61e
Luca Severo <luca98.severo@gmail.com>
2021-06-15 10:54:41 -0300

    removed console.logs

--- app/javascript/plugins/init_gifbox.js
+++ app/javascript/plugins/init_gifbox.js
@@ -4,7 +4,6 @@ const gifBtn = document.getElementById("gif-btn");
 const initGifBox = () => {
   if (gifBtn) {
     gifBtn.addEventListener('click', (event) => {
-      console.log("Yaaay!")
       frame.classList.toggle('hidden');
     });
   };
