349ccf557989ec6cf9ee9be4689d85d4f72f0337
Andre Menezes <djdedemenezes@gmail.com>
2021-06-15 14:57:05 -0300

    Style scroll is using pseudo class on css --webkit-scrollbar

--- app/javascript/chat/cleanInput.js
+++ app/javascript/chat/cleanInput.js
@@ -6,6 +6,8 @@ const cleanInputMessage = () => {
       input.addEventListener('keyup', (event) => {
         if(event.keyCode == 13){
           input.value = ""
+          const messageContainer = document.querySelectorAll('#messages')
+          messageContainer[0].lastElementChild.scrollIntoView()          
         }
       })
     })
