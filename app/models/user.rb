dd56d0b556cd9fda53d387a8a04ddb5de179cd4b
Danilo Castro <danilogcastro@hotmail.com>
2021-06-15 17:02:35 -0300

    added dependent destroy for genre in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :groups, dependent: :destroy
   has_many :subscriptions, dependent: :destroy
-  has_many :genres
+  has_many :genres, dependent: :destroy
   has_many :likes, dependent: :delete_all
 
   has_one_attached :photo
