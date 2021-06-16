780797aacb350de9844fa68708f3e9d44dc83ef3
Andre Menezes <djdedemenezes@gmail.com>
2021-06-16 14:00:50 -0300

    small refactor on openChat. New function replaceButton

--- app/javascript/chat/create_chat.js
+++ app/javascript/chat/create_chat.js
@@ -2,6 +2,15 @@ import Rails from '@rails/ujs';
 import { fetchChatroom } from './fetch_chatroom';
 import { openChat } from './open_chat';
 
+const replaceButton = () => {
+  const divAction = document.querySelector('.profile-action')
+  divAction.innerHTML = ""
+  divAction.innerHTML = `<a href="#" class="btn-open-chat">
+                          <i class="fas fa-comments"></i>
+                          Connect
+                        </a>`
+}
+
 const newChat = () => {
   const button = document.querySelector('.btn-msg')
   // console.log(button.dataset.id)
@@ -16,14 +25,7 @@ const newChat = () => {
         type: "post",
         data: `user_id=${button.dataset.id}`,
         success: function(data) {
-          button.classList.toggle('btn-msg')
-          delete button.dataset.remote
-          divAction = document.querySelector('.profile-action')
-          divAction.innerHTML = ""
-          divAction.innerHTML = `<a href="#" class="btn-open-chat">
-                                  <i class="fas fa-comments"></i>
-                                  Connect
-                                </a>`
+          replaceButton()
           // button.removeEventListener('click', newChat, false);
           console.log(button)
           openChat();
