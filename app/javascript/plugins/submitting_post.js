63d852c1d6804cceae7aed3f008d337ea0ae73ce
Andre Menezes <djdedemenezes@gmail.com>
2021-06-16 19:06:29 -0300

    when waiting for post request to finish we display some feedback

--- app/javascript/plugins/submitting_post.js
+++ app/javascript/plugins/submitting_post.js
@@ -5,13 +5,14 @@ const submittingPost = () => {
   //   postButton.value = "Test"
   //   
   // })
-
   const post = document.querySelector('#new_post');
   post.addEventListener('submit', (event) => {
     event.preventDefault();
-    console.log("submitting..");
-    const buttonText = document.getElementById('post-submit')
     document.querySelector('body').style.cursor = "wait"
+    const newPostContainer = document.querySelector('.post--container')
+    console.log(newPostContainer)
+    newPostContainer.innerHTML = ""
+    newPostContainer.innerHTML = "<img src='http://res.cloudinary.com/rent-an-instrument/image/upload/v1623880328/pebtsyvvxdpltxof18ja.png' alt='Submitting post' style='display: block; margin: 0 auto;' width='126px' height='126px'><br><p class='text-center'>We are processing your post</p>"
   })
 }
 
