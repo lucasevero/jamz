071bd025c28d0e4a2023c80ae5b569a5a63daba6
Andre Menezes <djdedemenezes@gmail.com>
2021-06-16 14:03:32 -0300

    cleaning code

--- app/javascript/chat/open_chat.js
+++ app/javascript/chat/open_chat.js
@@ -1,23 +1,11 @@
-// find the existing chat with current user and page_user_id
-// abrir o chat na coluna da direita
 const openChat = () => {
-  // get btn to redirect click
   const button = document.querySelector('.btn-open-chat')
   if (button) {
-    console.log(button)
     button.addEventListener('click', (event) => {
       event.preventDefault()
-      //  get username without @
-      // var mobileWithCode="+91-9842505145";//mobile value with nation code
       const pageUsername = document.querySelector('.username').innerText
-      console.log(pageUsername)
-      // get chat div username
       const chatRoom = document.querySelector(`div[data-user="${pageUsername}"]`)
-      console.log(chatRoom)
-      // Simulate click function
-      // function clickButton() {
       chatRoom.click();
-      // }
     })
   }
 }
