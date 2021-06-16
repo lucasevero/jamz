a4bdf3bf0d7788e62feacc5019065df7b8001f53
Andre Menezes <djdedemenezes@gmail.com>
2021-06-16 14:01:34 -0300

    cleanuing code

--- app/javascript/chat/create_chat.js
+++ app/javascript/chat/create_chat.js
@@ -13,24 +13,17 @@ const replaceButton = () => {
 
 const newChat = () => {
   const button = document.querySelector('.btn-msg')
-  // console.log(button.dataset.id)
-  // const button = document.querySelectorAll("a[data-id]")[0]
   if(button){
     button.addEventListener('click', (event) => {
       event.preventDefault();
-      // console.log(document.getElementsByName("csrf-token"))
-      // console.log(document.getElementsByName("csrf-token")[0].content)
       Rails.ajax({
         url: "/chatrooms",
         type: "post",
         data: `user_id=${button.dataset.id}`,
         success: function(data) {
           replaceButton()
-          // button.removeEventListener('click', newChat, false);
-          console.log(button)
           openChat();
           fetchChatroom()
-          // Se quiser abrir tem que achar e clicar no javascript
         },
         error: function(data) {}
       })
