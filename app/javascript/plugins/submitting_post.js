a27a3684e35affa1cd106205579ae0540f55fae1
Andre Menezes <djdedemenezes@gmail.com>
2021-06-17 13:37:05 -0300

    added if to check for element that has event listener

--- app/javascript/plugins/submitting_post.js
+++ app/javascript/plugins/submitting_post.js
@@ -6,14 +6,16 @@ const submittingPost = () => {
   //   
   // })
   const post = document.querySelector('#new_post');
-  post.addEventListener('submit', (event) => {
-    event.preventDefault();
-    document.querySelector('body').style.cursor = "wait"
-    const newPostContainer = document.querySelector('.post--container')
-    console.log(newPostContainer)
-    newPostContainer.innerHTML = ""
-    newPostContainer.innerHTML = "<img src='http://res.cloudinary.com/rent-an-instrument/image/upload/v1623880328/pebtsyvvxdpltxof18ja.png' alt='Submitting post' style='display: block; margin: 0 auto;' width='126px' height='126px'><br><p class='text-center'>We are processing your post</p>"
-  })
+  if(post){
+    post.addEventListener('submit', (event) => {
+      event.preventDefault();
+      document.querySelector('body').style.cursor = "wait"
+      const newPostContainer = document.querySelector('.post--container')
+      console.log(newPostContainer)
+      newPostContainer.innerHTML = ""
+      newPostContainer.innerHTML = "<img src='http://res.cloudinary.com/rent-an-instrument/image/upload/v1623880328/pebtsyvvxdpltxof18ja.png' alt='Submitting post' style='display: block; margin: 0 auto;' width='126px' height='126px'><br><p class='text-center'>We are processing your post</p>"
+    })
+  }
 }
 
 export { submittingPost }
