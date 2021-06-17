475295b975b854cc965937d97738177cd11435de
Andre Menezes <djdedemenezes@gmail.com>
2021-06-17 14:18:22 -0300

    banner typed js adjustments

--- app/javascript/plugins/banner_typed.js
+++ app/javascript/plugins/banner_typed.js
@@ -4,7 +4,7 @@ const loadDynamicBannerText = () => {
   const banner = document.querySelector('.banner')
   if (banner) {
     new Typed('#banner-typed-text', {
-      strings: ["beat.", "community.", "rhythm."],
+      strings: ["beat", "rhythm", "community"],
       typeSpeed: 75,
       loop: true
     });
